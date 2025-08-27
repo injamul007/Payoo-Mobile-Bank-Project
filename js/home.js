a35deb42d79aeb4960c847a67bd8d65a74144400
injamul007 <injamul007hoque@gmail.com>
2025-08-27 12:07:23 +0600

    update and add functionality to add amount in the app

--- js/home.js
+++ js/home.js
@@ -1,4 +1,16 @@
 //? Home page JS Code starts here
 
 
-
+document.getElementById('add_money_btn').addEventListener('click', function(e) {
+  e.preventDefault();
+  const bankName = document.getElementById('select_bank').value;
+  const bankAccountNumber = parseInt(document.getElementById('bank_account_number').value);
+  const amountToAdd = parseInt(document.getElementById('amount_to_add').value);
+  const pinNumber = parseInt(document.getElementById('pin_number').value);
+  
+  const availableBalance = parseInt(document.getElementById('available_balance').innerText);
+  
+  const totalNewAvailableBalance = availableBalance+amountToAdd;
+  document.getElementById('available_balance').innerText = totalNewAvailableBalance;
+  
+})
